02014b546b47afa3a3c722cc593be4e18ec5d222
SadiaBhaks <bhakssadia@gmail.com>
2025-10-04 22:20:18 +0600

    Update BASE_URL to include /api for backend

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -4,7 +4,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-BASE_URL = "https://backend-ecommerce-qdzy.onrender.com"
+BASE_URL = "https://backend-ecommerce-qdzy.onrender.com/api"
   
 
 
