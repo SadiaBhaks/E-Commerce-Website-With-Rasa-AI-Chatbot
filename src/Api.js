33267dc8a8b62d401ed7b1373a74f0327a8c9f63
SadiaBhaks <bhakssadia@gmail.com>
2025-10-04 20:07:14 +0600

    Update API base URL and Rasa WebChat socket for production

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 // src/api.js
 
-const API_BASE = "https://backend-ecommerce-qdzy.onrender.com/api";
+const API_BASE = "https://backend-ecommerce-qdzy.onrender.com/";
 
 // -------------------- Backend API --------------------
 
